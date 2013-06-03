d093ab54b061d5a169c194ce0279f281fd216c41
Jason Adam Young <jayoung@extension.org>
2013-06-03 17:06:17 -0400

    misc

--- evaluation/evaluation_questions.R
+++ evaluation/evaluation_questions.R
@@ -1,6 +1,4 @@
 # focus on public evaluations only
-public_submitted_questions = subset(aae_evaluation_2013.22, aae_evaluation_2013.22$has_extension_account == 'false')
-public_evaluation_eligible_questions = subset(public_submitted_questions, public_submitted_questions$evaluation_eligible == 'true')
 
 # histogram of 
-evaluation_histogram = hist(public_evaluation_eligible_questions$evaluation_count, breaks = 11, plot = FALSE)
+evaluation_histogram = hist(aae_evaluation$evaluation_count, breaks = 11, plot = FALSE)
